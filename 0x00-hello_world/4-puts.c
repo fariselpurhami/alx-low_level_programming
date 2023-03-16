66ff8dbbf264bb5b276e56e3fc68366e7ad40ee7
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-16 03:57:45 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - this is the main function
+ * 
+ * Return: always 0
+ */
+  int main(void)
+{
+	   Puts("\"Programming is like building a multilingual puzzle");
+           return (0);
+}
