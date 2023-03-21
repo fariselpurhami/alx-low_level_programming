f7ecac188967215ad616e88c955ab0dfd118f4b6
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-21 08:22:42 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
@@ -11,15 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		printf("+");
 		return (1);
-	} else if (n == 0)
-}
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
 	}
+	else if (n == 0)
+	{
+		printf("0")
+		return (0);
+	}
+	else
+	{
+		printf("-");
 		return (-1);
+	}
 }
