4fe6b718c0ea15bc2a923e205b6d134fbe13310c
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-23 04:39:01 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int divisor = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	while (n / divisor >= 10)
+		divisor *= 10;
+
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) % 10 + '0');
+		divisor /= 10;
+	}
+}
