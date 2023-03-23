b8edd87622b82a0ca43066406cc55c71fcdadbc6
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-22 22:57:29 -0700

    0

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,6 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-<<<<<<< HEAD
 	if (a >= b && a >= c)
 	{
 		largest = a;
@@ -25,20 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-=======
-    if (a >= b && a >= c)
-    {
-        largest = a;
-    }
-    else if (b >= a && b >= c)
-    {
-        largest = b;
-    }
-    else (c >= a && c >= b)
-    {
-        largest = c;
-    }
->>>>>>> f7e3805e9cc3703671c1ddedda99281fce66ccf8
-
+	
 	return (largest);
 }
