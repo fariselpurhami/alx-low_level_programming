fa3a35b773d2ea3d8d68159c49a0e0e4eed28394
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-03-23 07:48:52 +0200

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,41 +1,45 @@
 #include <stdio.h>
 #include "main.h"
+/**
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
 void print_remaining_days(int month, int day, int year)
 {
-    int remaining_days = 0;
-    int is_leap_year = ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
-    if (is_leap_year && month > 2)
+    int days_left = 0;
+    int is_leap_year = 0;
+
+    /* Check if it's a leap year */
+    if (year % 4 == 0)
     {
-        day++;
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                is_leap_year = 1;
+            }
+        }
+        else
+        {
+            is_leap_year = 1;
+        }
     }
-    switch (month)
+
+    /* Calculate the number of days left in the year */
+    if (is_leap_year == 1 && month > 2)
     {
-        case 1:
-            remaining_days = 365 - day;
-            break;
-        case 2:
-            remaining_days = is_leap_year ? 366 - day : 365 - day;
-            break;
-        case 3:
-            remaining_days = is_leap_year ? 366 - 60 - day : 365 - 60 - day;
-            break;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            remaining_days = 30 * (month - 1) - day + 212;
-            break;
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            remaining_days = 31 * (month - 1) - day + 212;
-            break;
-        default:
-            printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-            return;
+        days_left = 366 - day;
     }
+    else
+    {
+        days_left = 365 - day;
+    }
+
+    /* Print the results */
     printf("Day of the year: %d\n", day);
-    printf("Remaining days: %d\n", remaining_days);
+    printf("Remaining days: %d\n", days_left);
 }
