64cbabc5e1202954a078a1bab1987f89f33da074
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-23 09:42:33 -0700

    Numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,42 +6,26 @@
  */
 void print_number(int n)
 {
-	int i, digit, digits, is_negative;
-	unsigned int power;
+	int i, j, k, m;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
 	if (n < 0)
 	{
-		is_negative = 1;
+		_putchar('-');
 		n = -n;
 	}
-	else
+	i = n;
+	j = 1;
+	while (i > 9)
 	{
-		is_negative = 0;
+		j *= 10;
+		i /= 10;
 	}
-
-	digits = 0;
-	for (i = n; i > 0; i /= 10)
+	k = n;
+	while (j >= 1)
 	{
-		digits++;
-	}
-	power = 1;
-	for (i = 1; i < digits; i++)
-	{
-		power *= 10;
-	}
-	if (is_negative)
-	{
-		_putchar('-');
-	}
-	for (i = digits; i > 0; i--)
-	{
-		digit = (n / power) % 10;
-		_putchar(digit + '0');
-		power /= 10;
+		m = k / j;
+		_putchar(m + '0');
+		k -= m * j;
+		j /= 10;
 	}
 }
