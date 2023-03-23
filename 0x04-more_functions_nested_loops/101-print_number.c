570b77321b70ba581304d03d845f5817eeef1cbd
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-23 04:55:36 -0700

    Numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,44 @@
 #include "main.h"
-
 /**
  * print_number - prints an integer
- * @n: integer to be printed
- *
+ * @n: integer to print
  * Return: void
  */
 void print_number(int n)
 {
-	int divisor = 1;
+	int i, j, digit, digits, power, is_negative;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	if (n < 0)
 	{
-		_putchar('-');
-		n *= -1;
+		is_negative = 1;
+		n = -n;
 	}
-
-	while (n / divisor >= 10)
-		divisor *= 10;
-
-	while (divisor != 0)
+	else
 	{
-		_putchar((n / divisor) % 10 + '0');
-		divisor /= 10;
+		is_negative = 0;
+	}
+	digits = 0;
+	for (i = n; i > 0; i /= 10)
+	{
+		digits++;
+	}
+	for (i = 0; i < digits; i++)
+	{
+		power = 1;
+		for (j = 0; j < digits - i - 1; j++)
+		{
+			power *= 10;
+		}
+		digit = (n / power) % 10;
+		_putchar(digit + '0');
+	}
+	if (is_negative)
+	{
+		_putchar('-');
 	}
 }
