313c1c15be73c150a7ab6a622f2c35d896754022
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-27 09:45:50 -0700

    dont hate the hacker

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that generates random
+ * vaild passwords for the program 101-crackme.
+ *
+ * Return: 0 Always
+ */
+
+#define PASSWORD_LENGTH 64
+
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1] = {0};
+	const char charset[] =
+		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const int charset_length = sizeof(charset) - 1;
+
+	srand(time(NULL));
+
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+
+		password[i] = charset[rand() % charset_length];
+	}
+	printf("%s", password);
+
+	return (0);
+}
