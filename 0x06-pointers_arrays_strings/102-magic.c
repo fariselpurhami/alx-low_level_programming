ffc8c9806f506070ac2555f2a338add19401875c
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-30 03:58:42 -0700

    a

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - one line to this code
+ * main - Entry point for the program
  * You are not allowed to use the variable a
  * You are not allowed to modify the variable p
  * You can only write one statement
  * You are not allowed to use ,
  * You are not allowed to code anything than the line of expected line of code
+ * Return: Always (0)
  */
 int main(void)
 {
