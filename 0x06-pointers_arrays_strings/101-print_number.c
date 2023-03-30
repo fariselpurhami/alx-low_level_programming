dfd8bab4ac8bdd571adc692c3c1d316ef2b925ca
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-30 12:43:36 -0700

    n

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,7 +14,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		printf("-");
+		_putchar('-');
 		n1 = -n;
 	}
 
@@ -22,5 +22,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	printf("%c", (n1 % 10) + '0');
+	_putchar("%c", (n1 % 10) + '0');
 }
