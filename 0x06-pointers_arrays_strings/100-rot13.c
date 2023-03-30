c985f59b6c0dc44f2a5593aab0f5e17af0740209
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-30 04:40:06 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,29 +2,23 @@
 
 /**
  * rot13 - encodes a string using rot13.
- * @str: string to be encoded
+ * @s: string to be encoded
  *
  * Return: encoded string
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int i, j;
-	char *p = str;
+	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	char *p = s;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			for (j = 0; j < 13; j++)
-			{
-				if (str[i] == 'Z')
-					str[i] = 'A';
-				else if (str[i] == 'z')
-					str[i] = 'a';
-				else
-					str[i]++;
-			}
-		}
+		if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
+			s[i] += 13;
+		else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+			s[i] -= 13;
 	}
+
 	return (p);
 }
