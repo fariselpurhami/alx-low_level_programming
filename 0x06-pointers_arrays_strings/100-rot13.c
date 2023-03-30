cb69b773e723a14da161b3b38631f6ac0c56d6ad
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-30 00:51:55 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,25 +9,29 @@
 char *rot13(char *str)
 {
 	int j;
-	char *result = s;
-	char *ptr = str;
 
-	for (i = 0; s[i] != '\0'; i++)
+	char *result = str;
+
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for
-			("j = 0; (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'); j++")
+		for (j = 0;
+				((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A'
+					&& str[i] <= 'Z'))
+				&& j < 13;
+				j++)
 		{
 			if (j == 13)
 			{
 				break;
 			}
-			if ((s[i] >= 'a' && s[i] < 'n') || (s[i] >= 'A' && s[i] < 'N'))
+			if ((str[i] >= 'a' && str[i] < 'n') || (str[i] >= 'A' && str[i] < 'N'))
 			{
-				s[i] += 13;
+				str[i] += 13;
 			}
 			else
 			{
-				s[i] -= 13;
+				str[i] -= 13;
 			}
 		}
 	}
