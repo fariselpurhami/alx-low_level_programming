3a6f67dc3c8b95a1774c62e943e7a68d0d983e91
fariselpurhami <fares.alternate2014@gmail.com>
2023-03-30 13:02:11 -0700

    it

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,34 +13,37 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, i, j, k, carry, sum;
+	int i, j, k, l, m, n;
 
-	len1 = strlen(n1);
-	len2 = strlen(n2);
-
-	if (len1 > size_r || len2 > size_r)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-
-	i = len1 - 1;
-	j = len2 - 1;
-	k = size_r - 1;
-	carry = 0;
-
-	while (i >= 0 || j >= 0 || carry > 0)
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		sum = carry;
+		n = m;
 		if (i >= 0)
-			sum += n1[i--] - '0';
+			n += n1[i] - '0';
 		if (j >= 0)
-			sum += n2[j--] - '0';
-		if (k < 0)
-			return (0);
-		r[k--] = (sum % 10) + '0';
-		carry = sum / 10;
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-
-	if (k < 0)
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
 		return (0);
-
-	return (r + k + 1);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
