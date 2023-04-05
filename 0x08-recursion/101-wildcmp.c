07086ffff06efa177e92326b35493a3cbbe64dad
fariselpurhami <fares.alternate2014@gmail.com>
2023-04-05 08:35:10 -0700

    ide

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,9 +2,12 @@
 
 /**
  * wildcmp - function that compares two strings and returns 1 if the strings.
+ * are identical or if the second string contain a wild card character
+ * (*) that matches any sequences of strings
  * @s1: 1 string
  * @s2: 2 string
- * Return: identical s1, s2
+ * Return: if s1 , s2 are identical or s2 contain wildcard
+ * that matches s1, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -23,5 +26,5 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s1 != *s2)
 		return (0);
-	return (1);
+	return (wildcmp(s1 + 1, s2 + 1));
 }
