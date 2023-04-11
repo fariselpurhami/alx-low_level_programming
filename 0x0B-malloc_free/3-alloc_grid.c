95f6540e32939a54a251b501ad1374cda70f464b
fariselpurhami <fares.alternate2014@gmail.com>
2023-04-11 13:25:45 -0700

    If you even dream of beating me

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,18 +20,23 @@ int **alloc_grid(int width, int height)
 
 	grid = malloc(height * sizeof(int *));
 
-	if (grid[i] == NULL)
+	if (grid == NULL)
 	{
-		for (j = 0; j < i; j++)
-		{
-			free(grid[j]);
-		}
-		free(grid);
 		return (NULL);
 	}
-	for (j = 0; j < width; j++)
+	for (i = 0; i < height; i++)
 	{
-		grid[i][j] = 0;
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
+
 	return (grid);
 }
