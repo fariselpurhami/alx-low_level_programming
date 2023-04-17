b9e43f3e88224580906e02c67124eb4b7de8626b
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-04-17 16:57:53 +0200

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,13 +2,13 @@
 #include <stddef.h>
 
 /**
- * init_dog - WRITE A FUNCTION THAT INITIALIZE A VARIABLE OF TYPE STRUCT DOG
- * @d: STRUCT A DOG TO POINTER
- * @name: NAME OF THE DOG
- * @age: AGE OF THE DOG
- * @owner: OWNER OF THE DOG
+ * init_dog - WRITE A FUNCTION THAT INITIALIZE A VARIABLE OF TYPE STRUCT DOG.
+ * @d: STRUCT A DOG TO POINTER.
+ * @name: NAME OF THE DOG.
+ * @age: AGE OF THE DOG.
+ * @owner: OWNER OF THE DOG.
  *
- * Return: VOID
+ * Return: VOID.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
