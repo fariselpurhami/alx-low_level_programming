4064f6dcd9373a33074431669a012c3a8521a7ae
fariselpurhami <fares.alternate2014@gmail.com>
2023-04-19 15:29:29 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
 
 	num_bytes = atoi(argv[1]);
@@ -23,14 +23,17 @@ int main(int argc, char **argv)
 	if (num_bytes < 0)
 	{
 		printf("Error\n");
-		exit(2);
+		return (2);
 	}
 
-	for (i = 0; i < num_bytes; i++)
+	unsigned char *main_ptr = (unsigned char *)main;
+
+	for (i = 0; i < num_bytes - 1; i++)
 	{
-		printf("%02x ", ((unsigned char *)main)[i]);
-	}
-	printf("\n");
+		printf("%02x ", main_ptr[i]);
 
-	return (0);
+		printf("%02x\n", main_ptr[i]);
+
+		return (0);
+	}
 }
