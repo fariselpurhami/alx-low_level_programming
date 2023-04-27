17b91871de24b0ac5911b8103532277e06b0b546
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-04-27 20:30:43 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 		{
 			printf("[0] (nil)\n");
 		}
-		if (ptr->str != NULL)
+		else if (ptr->str != NULL)
 		{
 			printf("[%d] %s\n", ptr->len, ptr->str);
 		}
