5bc30fe9d782f0e176ced06b69f5940bda30c200
fariselpurhami <fares.alternate2014@gmail.com>
2023-04-27 06:33:52 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,21 +9,22 @@
  */
 size_t print_list(const list_t *h)
 {
+	const list_t *ptr = h;
 	size_t count = 0;
 
-	while (h != NULL)
+	while (ptr != NULL)
 	{
-		if (h->str == NULL)
+		if (ptr->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", ptr->len, ptr->str);
 		}
 
 		count++;
-		h = h->next;
+		ptr = ptr->next;
 	}
 
 	return (count);
