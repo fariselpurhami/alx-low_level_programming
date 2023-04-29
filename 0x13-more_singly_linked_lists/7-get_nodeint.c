ab5f0391b5bfb348f16ca1b7bdc65c9a74a643d8
fariselpurhami <fares.alternate2014@gmail.com>
2023-04-29 08:43:03 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,45 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - LET'S WRITE A FUNCTION CAN
+ * RETURNS THE NTH NODES OF A LISTINT_T LINKED LISTS.
+ * @head: IT'S LIST OF THE POINTERS NODE IN THE LIST.
+ * @index: RETURNING THE INDEX OF THE NODES AT ZEROO.
+ * Return: NTH OF POINTER,NULL IF IT WILL NOT EXISTS.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+	unsigned int i = 0;
+
+	while (current != NULL && i < index)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+		i++;
+	}
+
+	if (i == index && current != NULL)
+	{
+		current->next = prev;
+		return (current);
+	}
+
+	if (prev != NULL)
+	{
+		while (prev != NULL)
+		{
+			next = prev->next;
+			prev->next = current;
+			current = prev;
+			prev = next;
+		}
+	}
+
+	return (NULL);
+}
