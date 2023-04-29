0bb4b40068da42b330558a133a275649fc1a5449
fariselpurhami <fares.alternate2014@gmail.com>
2023-04-28 20:59:46 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - LET'S WRITE A FUNCTION THAT FREES LISTINT_T.
+ * @head: IT'S A LIST OF POINTERS TO LISTINT_T OF THE HEADERS.
+ * Return: NOTHING WILL BE RETURNED TO THE FUNCTIONS OF LISTS.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
