130eb354582a64d36a129806c2a3a0d545c33e9c
fariselpurhami <fares.alternate2014@gmail.com>
2023-05-01 11:10:57 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,7 +10,8 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow = head, *fast = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
 	while (slow && fast && fast->next)
 	{
