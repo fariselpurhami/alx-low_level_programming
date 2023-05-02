d435fa8f9492203f33067a679398f75b20d926e9
fariselpurhami <fares.alternate2014@gmail.com>
2023-05-01 22:45:58 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -12,7 +12,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *tortoise = head, *here = head;
 
-	if (head == NULL && head->next == NULL)
+	if (head != NULL)
 	{
 		return (NULL);
 	}
