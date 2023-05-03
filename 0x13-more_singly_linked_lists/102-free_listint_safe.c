23cc4f44a991d38c92097b4c2d65651a504e8d35
fariselpurhami <fares.alternate2014@gmail.com>
2023-05-02 23:37:22 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,6 +7,45 @@ size_t looped_listint_count(listint_t *head);
 size_t free_listint_safe(listint_t **h);
 
 /**
+ * looped_listint_count - LET'S WRITE A FUNCTIONS THAT PRINTS THE
+ * COUNT NUMBERS OF UNIQUES NODES IN LOOPED LISTINT_T LINKED LIST.
+ * @head: IT'S A LIST OF THE POINTER IN THE HEADER IN THE LISTINT.
+ * Return: THE LIST IF IT NOT LOOPED - 0 OTHERWISE IN UNIQE NODES.
+ */
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+	size_t count = 0;
+
+	if (head == NULL)
+	{
+		return (count);
+	}
+	tortoise = hare = head;
+
+	while (tortoise && hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
+
+	if (tortoise != hare)
+	{
+		tortoise = tortoise->next;
+		count++;
+
+		while (tortoise != hare)
+		{
+			count++;
+			tortoise = tortoise->next;
+		}
+
+		return (count);
+	}
+	return (count);
+}
+
+/**
  * free_listint_safe - LETS WRITE FUNCTION THAT FREES LISTINT_T.
  * @h: IT'S LIST OF THE POINTERS TO THE HEADER OF THE LISTINT_T.
  * Return: THE SIZE OF LIST THAT WAS FREED AND WILL BE RETURNED.
@@ -34,7 +73,7 @@ size_t free_listint_safe(listint_t **h)
 
 	next = current->next;
 	current->next = NULL;
-	free_listint(*h);
+	free(*h);
 	*h = next;
 	size++;
 
@@ -47,45 +86,4 @@ size_t free_listint_safe(listint_t **h)
 	}
 
 	return (size);
-}
-
-/**
- * looped_listint_count - LET'S WRITE A FUNCTIONS THAT PRINTS THE
- * COUNT NUMBERS OF UNIQUES NODES IN LOOPED LISTINT_T LINKED LIST.
- * @head: IT'S A LIST OF THE POINTER IN THE HEADER IN THE LISTINT.
- * Return: THE LIST IF IT NOT LOOPED - 0 OTHERWISE IN UNIQE NODES.
- */
-size_t looped_listint_count(listint_t *head)
-{
-	listint_t *tortoise, *hare;
-	size_t count = 0;
-
-	if (head == NULL)
-	{
-		return (count);
-	}
-
-	tortoise = hare = head;
-
-	while (tortoise && hare && hare->next)
-	{
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-	}
-
-	if (tortoise != hare)
-	{
-		tortoise = tortoise->next;
-		count++;
-
-		while (tortoise != hare)
-		{
-			count++;
-			tortoise = tortoise->next;
-		}
-
-		return (count);
-	}
-
-	return (count);
 }
