12d2d1de7abd0e1ebdb014b703472192e80644da
fariselpurhami <fares.alternate2014@gmail.com>
2023-05-04 10:56:09 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * LET'S WRITE A FUNCTIONS THAT RETURNS THE NUMBERS OF BITS
+ * YOU WOULD NEED TO FIIP TO GET FROM ONE NUMBER TO ANOTHER.
+ * @n: IT'S (N) THE  NUMBERS OF THE POINTERS TO THE HEADERS.
+ * @m: IT'S (M) THE NUMBERS TO BE COUNTED IN THE POINTERS_T.
+ * Return: COUNT
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_result = n ^ m;
+	unsigned int count = 0;
+
+	while(xor_result)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
+
+	return count;
+}
