0a7fc968441bffbf90343fcac42229713b44431d
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-05-05 07:23:25 +0300

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,13 +3,14 @@
 #include "main.h"
 
 /**
- * LET'S WRITE A FUNCTIONS THAT RETURNS THE NUMBERS OF BITS
+ * flip_bits - LETS WRITE FUNCTION RETURN THE NUMBER OF BIT
  * YOU WOULD NEED TO FIIP TO GET FROM ONE NUMBER TO ANOTHER.
  * @n: IT'S (N) THE  NUMBERS OF THE POINTERS TO THE HEADERS.
  * @m: IT'S (M) THE NUMBERS TO BE COUNTED IN THE POINTERS_T.
  * Return: COUNT
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n, 
+unsigned long int m)
 {
 	unsigned long int xor_result = n ^ m;
 	unsigned int count = 0;
@@ -20,5 +21,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		xor_result >>= 1;
 	}
 
-	return count;
+	return (count);
 }
