14d1596e5893d07231a66aa869c1934aa494ffc2
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-05-05 07:24:59 +0300

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -15,7 +15,7 @@ unsigned long int m)
 	unsigned long int xor_result = n ^ m;
 	unsigned int count = 0;
 
-	while(xor_result)
+	while (xor_result)
 	{
 		count += xor_result & 1;
 		xor_result >>= 1;
