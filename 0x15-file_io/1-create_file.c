67ffeea4b623658444a49246a223e5153584c682
fariselpurhami <fares.alternate2014@gmail.com>
2023-05-08 07:23:47 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+int create_file(const char *filename, char *text_content);
+
+/**
+ * create_file - IT'S FUNCTION THAT CREAT A FILE.
+ * @filename: IT'S CREATING THE NAME OF THE FILE.
+ * @text_content: IT'S WRITING THE CONTENTS FILE.
+ * Return: ALWAYS 1 ON SUCCESS,ALW -1 ON FAILURE.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, res, len = 0;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd == -1)
+	{
+		perror("open");
+		return (-1);
+	}
+
+	res = write(fd, text_content, len);
+	if (res == -1)
+	{
+		perror("write");
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
