0bb95fb415037d308f478e2bc535f4305a6bb38a
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-05-09 13:27:11 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, res;
 	size_t len = 0;
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
 
 	if (!filename)
 	{
