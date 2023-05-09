48a1eafde492e7aeb40adee66824d85eb51761a8
fariselpurhami <fares.alternate2014@gmail.com>
2023-05-09 02:18:58 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,30 +40,30 @@ int main(int argc, char *argv[])
 	while ((read_count = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		write_count = write(file_to, buffer, read_count);
+	}
 
-		if (write_count != read_count)
-		{
-			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-			exit(99);
-		}
+	if (write_count != read_count)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+		exit(99);
+	}
 
-		if (read_count == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+	if (read_count == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-		if (close(file_from) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
-			exit(100);
-		}
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
+		exit(100);
+	}
 
-		if (close(file_to) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_to);
-			exit(100);
-		}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_to);
+		exit(100);
 	}
 
 	return (0);
