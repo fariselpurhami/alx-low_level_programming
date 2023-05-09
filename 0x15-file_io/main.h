91724dda391725e6804943dbb7002f6a977b8623
fariselpurhami <fares.alternate2014@gmail.com>
2023-05-08 23:27:19 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <ernno.h>
+#include <errno.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
