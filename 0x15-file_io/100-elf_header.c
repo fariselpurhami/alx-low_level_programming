297fcbcbe03ab21baf68dbbf91025cdad22a76e6
fariselpurhami <fares.alternate2014@gmail.com>
2023-05-10 02:24:39 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+#define BUF_SIZE 128
 #define MAX_FILENAME_LENGHT 256
 #define MAX_TEXT_CONTENT_LENGHT 1024
 
@@ -27,7 +28,7 @@ int main(int argc, char **argv)
 {
 	int fd, n;
 	char buf[128];
-	Elf64_Ehdr *elf_header;
+	Elf64_Ehdr *ehdr;
 
 	if (argc != 2)
 	{
@@ -46,49 +47,37 @@ int main(int argc, char **argv)
 		print_error("Failed to read file");
 	}
 
-	elf_header = (Elf64_Ehdr *) buf;
-
-	if (elf_header->e_ident[EI_MAG0] != ELFMAG0 ||
-			elf_header->e_ident[EI_MAG1] != ELFMAG1 ||
-			elf_header->e_ident[EI_MAG2] != ELFMAG2 ||
-			elf_header->e_ident[EI_MAG3] != ELFMAG3)
-	{
-		print_error("File is not an ELF file");
-	}
+	ehdr = (Elf64_Ehdr *) buf;
 
 	printf("ELF Header:\n");
-	printf("  Magic:   %02lx %02lx %02lx %02lx %02lx %02lx %02lx %02lx\n"
-			"%02x %02x %02x %02x %02x %02x %02x %lx\n",
-			elf_header->e_ident[EI_MAG0],
-			elf_header->e_ident[EI_MAG1],
-			elf_header->e_ident[EI_MAG2],
-			elf_header->e_ident[EI_MAG3],
-			elf_header->e_ident[EI_CLASS],
-			elf_header->e_ident[EI_DATA],
-			elf_header->e_ident[EI_VERSION],
-			elf_header->e_ident[EI_OSABI],
-			elf_header->e_ident[EI_ABIVERSION],
-			elf_header->e_ident[EI_PAD],
-			elf_header->e_type, elf_header->e_machine,
-			elf_header->e_version, elf_header->e_entry);
+	printf("  Magic:   %02lx %02lx %02lx %02lx %02lx %02lx %02lx %02lx\n",
+			(unsigned long) ehdr->e_ident[EI_MAG0],
+			(unsigned long) ehdr->e_ident[EI_MAG1],
+			(unsigned long) ehdr->e_ident[EI_MAG2],
+			(unsigned long) ehdr->e_ident[EI_MAG3],
+			(unsigned long) ehdr->e_ident[EI_MAG4],
+			(unsigned long) ehdr->e_ident[EI_MAG5],
+			(unsigned long) ehdr->e_ident[EI_MAG6],
+			(unsigned long) ehdr->e_ident[EI_MAG7]);
+
 	printf("  Class:                             %s\n",
-			elf_header->e_ident[EI_CLASS] ==
+			ehdr->e_ident[EI_CLASS] ==
 			ELFCLASS32 ? "ELF32" : "ELF64");
 	printf("  Data:                              %s\n",
-			elf_header->e_ident[EI_DATA] ==
+			ehdr->e_ident[EI_DATA] ==
 			ELFDATA2LSB ? "2's complement,little endian"
 			: "2's complement, big endian");
 	printf("  Version:                           %d (current)\n",
-			elf_header->e_version);
+			ehdr->e_version);
 	printf("  OS/ABI:                            %s\n",
-			elf_header->e_ident[EI_OSABI] ==
+			ehdr->e_ident[EI_OSABI] ==
 			ELFOSABI_SYSV ? "UNIX - System V" : "Unknown");
 	printf("  ABI Version:                       %d\n",
-			elf_header->e_ident[EI_ABIVERSION]);
+			ehdr->e_ident[EI_ABIVERSION]);
 	printf("  Type:                              %s\n",
-			elf_header->e_type ==
+			ehdr->e_type ==
 			ET_EXEC ? "EXEC (Executable file)" : "Unknown");
-	printf("  Entry point address:               0x%lx\n", elf_header->e_entry);
+	printf("  Entry point address:               0x%lx\n", ehdr->e_entry);
 
 	close(fd);
 	return (0);
