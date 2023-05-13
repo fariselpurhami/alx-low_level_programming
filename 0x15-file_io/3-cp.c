edf635721f32a9320c7631863a4c864b49768ee6
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-05-13 07:41:18 +0300

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 		w = write(fd_to, buf, r);
 		if (w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
