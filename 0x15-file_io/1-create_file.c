738e8d13494a27529b0c201135359068eb34b21b
fariselpurhami <fares.alternate2014@gmail.com>
2023-05-12 21:49:42 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -58,7 +58,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (res == -1)
 	{
-		perror("write");
+
+		dprintf(STDERR_ FILENO, "Error: Can't read from %s\n", filename);
 		close(fd);
 		return (-1);
 	}
