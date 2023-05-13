c3163b3b5bf6f9d5ea6a639be835dd4eca3435d9
fariselpurhami <fares.alternate2014@gmail.com>
2023-05-12 22:30:27 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,12 +29,6 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	if (chmod(filename, S_IRUSR | S_IWUSR) == -1)
-	{
-		perror("chmod");
-		return (-1);
-	}
-
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (fd == -1)
 	{
