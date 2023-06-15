7c6dd6be1e5170b2baef30dad240267039ec8d27
fariselpurhami <fares.alternate2014@gmail.com>
2023-06-14 23:11:39 -0700

    updated

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,15 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+/* headers */
+
+
+
+
+
+
+
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -18,7 +27,7 @@ typedef struct dlistint_s
 } dlistint_t;
 
 
-/*=========================================*/
+/*==========================================================================*/
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 /*==========================================================================*/
@@ -26,10 +35,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-/*=================================================================*/
+/*==========================================================================*/
 int sum_dlistint(dlistint_t *head);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-/*==================================*/
+/*==========================================================================*/
 void free_dlistint(dlistint_t *head);
 
 #endif /* LISTS_H */
