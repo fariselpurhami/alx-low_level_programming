55e0036e18dae4591dbf6443cb61fe0ca53d1ba9
fariselpurhami <fares.alternate2014@gmail.com>
2023-06-19 00:00:19 -0700

    updated

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ * @s: the string for search
+ * @accept: the substring for search
+ *
+ * Return: the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			return (count);
+		}
+	}
+	return (count);
+}
