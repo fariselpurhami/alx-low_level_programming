fe1b544bbbbfbc9c6b7965c69e8f8fcddfbf6c48
Faris <125440933+fariselpurhami@users.noreply.github.com>
2023-06-19 12:47:13 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp -q "https://github.com/fariselpurhami/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmmw.so"
-export LD_PRELOAD="/tmp/libmmw.so"
+wget -P /tmp/ https://raw.github.com/fariselpurhami/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
+export LD_PRELOAD=/tmp/preload.so
